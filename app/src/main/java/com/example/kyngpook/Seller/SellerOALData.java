2a8206bb2ffd498e68ff880d9d640c2c7ced9ed8
JEONGHYEONSEOK1 <55523075+JEONGHYEONSEOK1@users.noreply.github.com>
2020-06-19 22:30:41 +0900

    데이터 오류 수정

--- app/src/main/java/com/example/kyngpook/Seller/SellerOALData.java
+++ app/src/main/java/com/example/kyngpook/Seller/SellerOALData.java
@@ -3,8 +3,8 @@ package com.example.kyngpook.Seller;
 
 // 판매자 주문내역 상세보기 내에 요청상품 리스트를 위한 데이터타입
 public class SellerOALData {
-    private String name;
-    private String num;
+    String name;
+    String num;
 
     public SellerOALData(String name,String num)
     {
