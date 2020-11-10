50f4f58af7b908e79ad3f021611d322b16526ed2
GE60 <dhyun0601@naver.com>
2020-11-10 23:35:07 +0900

    201110 스플래시 액비티비, 앱 아이콘!

--- app/src/main/java/com/example/kyngpook/Login_Signup/StartActivity.java
+++ app/src/main/java/com/example/kyngpook/Login_Signup/StartActivity.java
@@ -22,6 +22,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -32,9 +33,6 @@ import static maes.tech.intentanim.CustomIntent.customType;
 
 
 public class StartActivity extends AppCompatActivity {
-    private ImageView bike;
-    private Animation anim;
-
     private LoginSharedPreferenceUtil util;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -51,74 +49,79 @@ public class StartActivity extends AppCompatActivity {
         util = new LoginSharedPreferenceUtil(this);
         final Boolean autoLogin = util.getBooleanData("AutoLogin", false);
 
-        bike = (ImageView) findViewById(R.id.start_bike);
-        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_bike);
-        anim.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
+        if (autoLogin) {
+            String Auth = util.getStringData("권한", "null");
+            final String ID = util.getStringData("ID", "null");
 
-            }
-
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                if (autoLogin) {
-                    String Auth = util.getStringData("권한", "null");
-                    final String ID = util.getStringData("ID", "null");
-
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
                     Toast.makeText(StartActivity.this, ID + " 님 로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show();
-
-                    if (Auth.equals("일반 이용자")) {
-                        db.collection("USERS").document("Buyer").collection("Buyer")
-                                .get()
-                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                        if (task.isSuccessful()) {
-                                            Intent intent = new Intent(getApplicationContext(), Buyer_MainActivity.class);
-                                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                                Log.d("LoginActivity.java", document.getId() + " => " + document.getData());
-                                                if (document.getId().equals(ID)) {
-                                                    intent.putExtra("ID", ID);
-                                                    intent.putExtra("PASSWORD", (String) document.getData().get("PASSWORD"));
-                                                    intent.putExtra("닉네임", (String) document.getData().get("닉네임"));
-                                                    intent.putExtra("이름", (String) document.getData().get("이름"));
-                                                    intent.putExtra("전화번호", (String) document.getData().get("전화번호"));
-                                                    startActivity(intent);
-                                                }
-                                                ;
-                                            }
-                                        } else {
-//                             Log.w("LoginActivity.java", "Error getting documents.", task.getException());
-                                        }
-                                    }
-                                });
-
-
-                    } else if (Auth.equals("판매자")) {
-                        Intent intent = new Intent(getApplicationContext(), SellerMainActivity.class);
-                        intent.putExtra("ID", ID);
-                        startActivity(intent);
-                    } else { // 배달자
-                        Intent intent = new Intent(getApplicationContext(), Deliver_MainActivity.class);
-                        intent.putExtra("ID", ID);
-                        startActivity(intent);
-                    }
-                    finish();
-                } else {
-                    Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
-                    startActivity(intent);
-                    customType(StartActivity.this, "bottom-to-up");
-                    finish();
                 }
+            }, 2000);
+
+            if (Auth.equals("일반 이용자")) {
+                db.collection("USERS").document("Buyer").collection("Buyer")
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    final Intent intent = new Intent(getApplicationContext(), Buyer_MainActivity.class);
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Log.d("LoginActivity.java", document.getId() + " => " + document.getData());
+                                        if (document.getId().equals(ID)) {
+                                            intent.putExtra("ID", ID);
+                                            intent.putExtra("PASSWORD", (String) document.getData().get("PASSWORD"));
+                                            intent.putExtra("닉네임", (String) document.getData().get("닉네임"));
+                                            intent.putExtra("이름", (String) document.getData().get("이름"));
+                                            intent.putExtra("전화번호", (String) document.getData().get("전화번호"));
+                                            new Handler().postDelayed(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    startActivity(intent);
+                                                    finish();}
+                                            }, 1500);
+                                        }
+
+                                    }
+                                } else {
+//                             Log.w("LoginActivity.java", "Error getting documents.", task.getException());
+                                }
+                            }
+                        });
+
+
+            } else if (Auth.equals("판매자")) {
+                final Intent intent = new Intent(getApplicationContext(), SellerMainActivity.class);
+                intent.putExtra("ID", ID);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                        finish();}
+                }, 2000);
+            } else { // 배달자
+                final Intent intent = new Intent(getApplicationContext(), Deliver_MainActivity.class);
+                intent.putExtra("ID", ID);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                        finish();}
+                }, 2000);
             }
 
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-        bike.startAnimation(anim);
-
+        } else {
+            final Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                    finish();}
+            }, 2000);
+            //customType(StartActivity.this, "bottom-to-up");
+        }
 
     }
 
